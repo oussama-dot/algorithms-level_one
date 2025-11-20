be11ce6f4977493f27a7a7e3550bac2009d0eec6
oussama-dot <eloussamafouah@gmail.com>
2025-11-20 13:05:54 +0100

    good

---
+++ Problem_#24_Validate_Age_Between_18_and_45.py
@@ -0,0 +1,6 @@
+age_input = int(input("please enter your age "))
+
+if age_input > 17 and  age_input < 45:
+    print("you have the right to enter ")
+else :
+    print("you are not allowed to enter ")
