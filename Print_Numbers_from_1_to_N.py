e6f4def9b1862129b3c0b4baabb3c40318098dac
oussama-dot <eloussamafouah@gmail.com>
2025-11-21 12:57:36 +0100

    yo

---
+++ Print_Numbers_from_1_to_N.py
@@ -0,0 +1,3 @@
+num = int(input("please enter your num"))
+for i in range(num):
+    print(i+1)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+age_input = int(input("please enter your age "))
+
+while age_input < 17 or  age_input > 45:
+    age_input = int(input("please enter your age "))
+else : 
+    print("you have the rigth to access ")
+     
